f48f0462ebf7ec267186305563da491833e6f7f8
William Páez <wpaez@cenditel.gob.ve>
2019-01-25 12:34:23 -0400

    Agregado detalle de ubicación geográfica para cada nivel de usuario y sepan a cual pertenecen

--- base/views.py
+++ base/views.py
@@ -24,27 +24,27 @@ class HomeTemplateView(TemplateView):
         elif profile.level == 1:
             national_level = NationalLevel.objects.get(profile=profile)
             context['texto1'] = 'Bienvenido ' + str(profile)
-            context['texto2'] = 'Usuario nivel nacional ' + str(national_level.country) + '. Este usuario permite registrar y monitoriar a los representantes de \
+            context['texto2'] = 'Usuario nivel nacional ' + str(national_level.country) + '. Este usuario permite registrar y monitorear a los representantes de \
             cada estado que pertenecen a dicho país'
         elif profile.level == 2:
             state_level = StateLevel.objects.get(profile=profile)
             context['texto1'] = 'Bienvenido ' + str(profile)
-            context['texto2'] = 'Usuario nivel estado ' + str(state_level.state) + '. Este usuario permite registrar y monitoriar a los representantes de \
+            context['texto2'] = 'Usuario nivel estado ' + str(state_level.state) + '. Este usuario permite registrar y monitorear a los representantes de \
             cada municipio que pertenecen a dicho estado'
         elif profile.level == 3:
             municipal_level = MunicipalLevel.objects.get(profile=profile)
             context['texto1'] = 'Bienvenido ' + str(profile)
-            context['texto2'] = 'Usuario nivel municipio ' + str(municipal_level.municipality) + '. Este usuario permite registrar y monitoriar a los representantes \
+            context['texto2'] = 'Usuario nivel municipio ' + str(municipal_level.municipality) + ', ' + str(municipal_level.municipality.state) + '. Este usuario permite registrar y monitorear a los representantes \
             de cada parroquia que pertenecen a dicho municipio'
         elif profile.level == 4:
             parish_level = ParishLevel.objects.get(profile=profile)
             context['texto1'] = 'Bienvenido ' + str(profile)
-            context['texto2'] = 'Usuario nivel parroquia ' + str(parish_level.parish) + '. Este usuario permite registrar y monitoriar a los representantes \
+            context['texto2'] = 'Usuario nivel parroquia ' + str(parish_level.parish) + ', ' + str(parish_level.parish.municipality) + ', ' + str(parish_level.parish.municipality.state)  + '. Este usuario permite registrar y monitorear a los representantes \
             de cada consejo comunal que pertenecen a dicha parroquia'
         elif profile.level == 5:
             communal_council_level = CommunalCouncilLevel.objects.get(profile=profile)
             context['texto1'] = 'Bienvenido ' + str(profile)
-            context['texto2'] = 'Usuario nivel consejo comunal ' + str(communal_council_level.communal_council) + '. Este usuario permite registrar, actualizar, eliminar y listar \
+            context['texto2'] = 'Usuario nivel consejo comunal ' + str(communal_council_level.communal_council) + ', ' + str(communal_council_level.communal_council.parish) + ', ' + str(communal_council_level.communal_council.parish.municipality) + ', ' + str(communal_council_level.communal_council.parish.municipality.state) + '. Este usuario permite registrar, actualizar, eliminar y listar \
             a todos los adultos mayores'
         return context
 
